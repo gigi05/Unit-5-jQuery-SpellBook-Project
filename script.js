6d35bce3841fa75294cee763f928c27825da3e97
gigi05 <43378754+gigi05@users.noreply.github.com>
2019-01-10 20:32:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+//Spell #1
+
+$("#money").click(function(){
+    $("#moneyyy").slideToggle();
+});
+
+
+//Spell #2
+
+$("#flowers").click(function(){
+    $("#flower").fadeToggle();
+});
+
+
+//Spell #3
+
+$("#kim").click(function(){
+    $("#kimP").toggle();
+});
+
+
+
